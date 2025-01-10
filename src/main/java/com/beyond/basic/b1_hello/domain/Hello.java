313550273bffc13017f6e40da7f4dafce950e9ec
jaeseok <wotjrdl98@naver.com>
2025-01-10 09:07:28 +0900

    striing1-1

--- src/main/java/com/beyond/basic/b1_hello/domain/Hello.java
+++ src/main/java/com/beyond/basic/b1_hello/domain/Hello.java
@@ -1,9 +1,10 @@
 package com.beyond.basic.b1_hello.domain;
 import lombok.*;
 
-@Getter
+//@Getter
 @NoArgsConstructor//기본생성자
 @AllArgsConstructor//모든매개변수있는 생성자
+@Data
 //@Data : Getter, Setter, toString메서드까지 포함하는 어노테이션
 public class Hello {
     private String name;
